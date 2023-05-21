6030aa141449a94b4e1fe96dbb149e498a28a5dc
Arseniy Tokarev <lupenchik2135@gmail.com>
2023-05-21 13:38:38 +0300

    Aded Play Screen

--- core/src/com/mygdx/game/GameLogic.java
+++ core/src/com/mygdx/game/GameLogic.java
@@ -1,31 +1,31 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.mygdx.game.Screens.PlayScreen;
 
-public class GameLogic extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class GameLogic extends Game {
+	public SpriteBatch batch;
+	public static final int V_WIDTH = 1960;
+	public static final int V_HEIGHT = 1280;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		setScreen(new PlayScreen(this));
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 23, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
